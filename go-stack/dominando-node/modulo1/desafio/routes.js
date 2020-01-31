688005780cb20626d68c510c47baedc78601b8a5
Alex Rabelo Ferreira <arf92@live.com>
2020-01-30 22:53:19 -0300

    add de midleware verificaId

--- go-stack/dominando-node/modulo1/desafio/routes.js
+++ go-stack/dominando-node/modulo1/desafio/routes.js
@@ -11,6 +11,20 @@ const projects = [{
         ]
     } 
 ];
+const projectNotFoundJson = { error: "Projeto não existe"};
+// Crie um middleware que será utilizado em todas rotas que recebem o ID do projeto nos parâmetros da URL que verifica se o projeto com aquele ID existe. Se não existir retorne um erro, caso contrário permita a requisição continuar normalmente;
+function verificaId (req, res, next) {
+    const { id } = req.params;
+    const index = findProjectIndex(id);
+    if (index >= 0){
+        req.project = projects[index];
+        req.index = index;
+
+        return next();
+    }
+
+    return res.status(404).json(projectNotFoundJson);
+ }
 
 server.use(express.json());
 
@@ -24,9 +38,12 @@ server.post("/projects", (req, res) => {
 
     return res.json(projects);
 });
-server.post("/projects/:id/tasks", (req, res) => {
+
+// server.use(verificaId);
+
+server.post("/projects/:id/tasks", verificaId, (req, res) => {
     const id = req.params.id;
-    const project = findProject(id);
+    const {project} = req;
     if (project){
         const {title } = req.body;
         project.tasks.push(title);
@@ -34,12 +51,12 @@ server.post("/projects/:id/tasks", (req, res) => {
         return res.json(projects);
     }
 
-    return res.status(404).json({ error: "Projeto não existe"});
+    return res.status(500).json(projectNotFoundJson);
 });
 
-server.put("/projects/:id", (req, res) => {
+server.put("/projects/:id", verificaId, (req, res) => {
     const id = req.params.id;
-    const project = findProject(id);
+    const {project} = req;
     if (project){
         const {title } = req.body;
         project.title = title;
@@ -47,30 +64,20 @@ server.put("/projects/:id", (req, res) => {
         return res.json(projects);
     }
 
-    return res.status(404).json({ error: "Projeto não existe"});
+    return res.status(500).json(projectNotFoundJson);
 });
 
-server.delete("/projects/:id", (req, res) => {
+server.delete("/projects/:id", verificaId, (req, res) => {
     const id = req.params.id;
-    const index = findProjectIndex(id);
+    const {index} = req;
     if (index >= 0){
         projects.splice(index, 1);
 
         return res.json(projects);
     }
 
-    return res.status(404).json({ error: "Projeto não existe"});
+    return res.status(500).json();
 });
-
- function findProject(id) {
-    for (const item of projects) {
-        if (item.id === id){
-            return item;
-        }
-    };
-
-     return null;
- }
 
  function findProjectIndex(id) {
     for (let i = 0; i < projects.length; i++) {
@@ -82,8 +89,6 @@ server.delete("/projects/:id", (req, res) => {
 
     return -1;
  }
-
- // Crie um middleware que será utilizado em todas rotas que recebem o ID do projeto nos parâmetros da URL que verifica se o projeto com aquele ID existe. Se não existir retorne um erro, caso contrário permita a requisição continuar normalmente;
 
 // Crie um middleware global chamado em todas requisições que imprime (console.log) uma contagem de quantas requisições foram feitas na aplicação até então;
  
