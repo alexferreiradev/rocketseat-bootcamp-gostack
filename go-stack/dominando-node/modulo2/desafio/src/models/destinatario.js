cf106b3b356395636f25c8d16ab287c7d848d34b
Alex Rabelo Ferreira <arf92@live.com>
2020-02-11 22:56:14 -0300

    adicao de model destinatario

--- go-stack/dominando-node/modulo2/desafio/src/models/destinatario.js
+++ go-stack/dominando-node/modulo2/desafio/src/models/destinatario.js
@@ -1,1 +1,15 @@
-"Destinatario model"
+import Model from ''
+
+class Destinatario extends Model {
+    constructor() {
+        this.nome = "";
+        this.endereco = {
+            rua: "",
+            numero: 0,
+            complemento: "",
+            estado: "GO",
+            cidade: "Goiania",
+            cep: "74823302"
+        }
+    }
+}
