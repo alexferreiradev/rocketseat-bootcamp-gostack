73ce3248580009387d0a49afd6555bed4a58f5c4
Alex Rabelo Ferreira <arf92@live.com>
2020-02-24 10:46:07 -0300

    Adicao de route status

--- go-stack/dominando-node/modulo2/desafio/src/routes.js
+++ go-stack/dominando-node/modulo2/desafio/src/routes.js
@@ -1,8 +1,14 @@
 import {Router} from 'express';
 
 import UserController from './app/controllers/UserController';
+import DestinatarioController from './app/controllers/DestinatarioController';
+import config from './version';
 
 const routes = new Router();
+
+routes.get('/', (_, res) => {
+    return res.json({ status: "ok", version: config.version});
+});
 
 routes.post('/users', UserController.store);
 
