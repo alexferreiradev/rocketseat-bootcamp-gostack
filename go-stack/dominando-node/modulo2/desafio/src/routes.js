41c30ee921565f79e10532113a44fae240ab8b4e
Alex Rabelo Ferreira <arf92@live.com>
2020-02-25 12:05:53 -0300

    Adicao de middleware auth

--- go-stack/dominando-node/modulo2/desafio/src/routes.js
+++ go-stack/dominando-node/modulo2/desafio/src/routes.js
@@ -3,6 +3,7 @@ import {Router} from 'express';
 import UserController from './app/controllers/UserController';
 import DestinatarioController from './app/controllers/DestinatarioController';
 import SessionController from './app/controllers/SessionController';
+import authMid from './app/middlewares/auth';
 import config from './version';
 
 const routes = new Router();
@@ -10,9 +11,11 @@ const routes = new Router();
 routes.get('/', (_, res) => {
     return res.json({ status: "ok", version: config.version});
 });
+routes.post('/login', SessionController.login);
+
+routes.use(authMid);
 
 routes.post('/user', UserController.store);
-routes.post('/login', SessionController.login);
 
 routes.get('/recipient', DestinatarioController.index);
 routes.post('/recipient', DestinatarioController.store);
