40f5a33a2cc91eaa8261e77a5b1e8e92210bf76c
Alex Rabelo Ferreira <arf92@live.com>
2020-03-13 23:15:32 -0300

    Add upload de arquivos

--- go-stack/fase-2_go-barber/go-barber/src/routes.js
+++ go-stack/fase-2_go-barber/go-barber/src/routes.js
@@ -1,13 +1,16 @@
 import {Router} from 'express';
+import multer from 'multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMid from './app/middlewares/auth';
 import config from './version';
+import multerConfig from './config/multer';
 
 const routes = new Router();
 const store = UserController.store.bind(UserController);
 const update = UserController.update.bind(UserController);
+const upload = multer(multerConfig);
 
 routes.get('/', (_, res) => {
     return res.json({ status: "ok", version: config.version});
@@ -18,5 +21,9 @@ routes.use(authMid);
 
 routes.post('/user', store);
 routes.put('/user', update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+    return res.json({ ok: true});
+});
 
 export default routes;
