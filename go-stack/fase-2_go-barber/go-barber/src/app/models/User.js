e845293ffa50c3c8f690c776f87f3ab1bf4b1f25
Alex Rabelo Ferreira <arf92@live.com>
2020-03-15 23:23:24 -0300

    Add associacao de user e file

--- go-stack/fase-2_go-barber/go-barber/src/app/models/User.js
+++ go-stack/fase-2_go-barber/go-barber/src/app/models/User.js
@@ -25,6 +25,12 @@ class User extends Model {
         return this;
     }    
 
+    static associate(models) {
+        this.belongsTo(models.File, {
+            foreignKey: 'avatar_id'
+        });
+    }
+
     async checkPassword(pass) {
         return bcrypt.compare(pass, this.password_hash);
     }
