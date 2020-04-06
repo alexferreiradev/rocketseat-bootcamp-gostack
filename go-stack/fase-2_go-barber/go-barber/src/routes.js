04e5afe8762dd21a9c668558483d3a4baf2ca95a
Alex Rabelo Ferreira <arf92@live.com>
2020-04-05 21:56:45 -0300

    Adicao de provider controller

--- go-stack/fase-2_go-barber/go-barber/src/routes.js
+++ go-stack/fase-2_go-barber/go-barber/src/routes.js
@@ -4,6 +4,7 @@ import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 import authMid from './app/middlewares/auth';
 import config from './version';
 import multerConfig from './config/multer';
@@ -20,9 +21,13 @@ routes.post('/login', SessionController.login);
 
 routes.use(authMid);
 
+routes.get('/user', UserController.index);
 routes.post('/user', store);
 routes.put('/user', update);
 
-routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/provider', ProviderController.index);
+
+routes.post('/file', upload.single('file'), FileController.store);
+//routes.get('/file', FileController.find);
 
 export default routes;
