ed1b02a7863ed85ca0dda7d8b2559ca2577824dd
Alex Rabelo Ferreira <arf92@live.com>
2020-04-08 21:52:52 -0300

    Adicao de controller para appointment

--- go-stack/fase-2_go-barber/go-barber/src/routes.js
+++ go-stack/fase-2_go-barber/go-barber/src/routes.js
@@ -5,6 +5,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 import authMid from './app/middlewares/auth';
 import config from './version';
 import multerConfig from './config/multer';
@@ -26,6 +27,8 @@ routes.post('/user', store);
 routes.put('/user', update);
 
 routes.get('/provider', ProviderController.index);
+routes.get('/appointment', AppointmentController.index);
+routes.post('/appointment', AppointmentController.store);
 
 routes.post('/file', upload.single('file'), FileController.store);
 routes.get('/file', FileController.index);
