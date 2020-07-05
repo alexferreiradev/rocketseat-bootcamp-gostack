3a6c03e912aa7f48e959cef6a2da9062603a0e29
Alex Rabelo Ferreira <arf92@live.com>
2020-07-05 18:38:51 -0300

    handles mob

--- go-stack/desafio-final/mobile/src/pages/Dashboard/index.js
+++ go-stack/desafio-final/mobile/src/pages/Dashboard/index.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Keyboard, Text } from 'react-native';
 
@@ -34,110 +35,128 @@ import {
   DetalhesButtonText,
 } from './styles';
 
-class Dashboard extends Component {
-  constructor() {
-    super();
+import { doLogoutReq } from '../../store/modules/user/actions';
+
+function Dashboard({ navigation }) {
+  const dispatch = useDispatch();
+  const { id, nome } = useSelector((state) => state.user);
+  const [encomendaList, setEncomendaList] = useState([]);
+
+  function handleExit() {
+    dispatch(doLogoutReq());
   }
 
-  handleDetalhes = (item) => {
-    const { navigation } = this.props;
+  function handleChangeFilter(type) {
+    switch (type) {
+      case 'entregue': {
+        fetchData('status=entregue');
+        break;
+      }
+      case 'pendente':
+        fetchData('status_ne=entregue');
+        break;
+    }
+  }
 
+  function handleDetalhes(item) {
     navigation.navigate('Encomenda', { id: item.id });
 
     Keyboard.dismiss();
-  };
-
-  render() {
-    return (
-      <Container>
-        <Header>
-          <Image>
-            <ImageText>GA</ImageText>
-          </Image>
-          <Welcome>
-            <Text>Bem vindo de volta,</Text>
-            <NameText>Gaspar Antunes</NameText>
-          </Welcome>
-          <Icon name="exit-to-app" color="#E74040" size={24} />
-        </Header>
-
-        <EntregaList>
-          <ListHeader>
-            <ListTitle>Entregas</ListTitle>
-            <ListFilter>
-              <FilterText>Pendentes</FilterText>
-              <FilterText>Entregues</FilterText>
-            </ListFilter>
-          </ListHeader>
-          <List
-            data={[
-              {
-                id: 1,
-                status: 'entregue',
-                data: '14/01/2020',
-                cidade: 'Goiânia',
-              },
-              {
-                id: 2,
-                status: 'entregue',
-                data: '14/01/2020',
-                cidade: 'Goiânia',
-              },
-              {
-                id: 3,
-                status: 'entregue',
-                data: '14/01/2020',
-                cidade: 'Goiânia',
-              },
-            ]}
-            keyExtractor={(encomenda) => encomenda.id}
-            renderItem={({ item }) => (
-              <Encomenda>
-                <InfoHeader>
-                  <Icon name="local-shipping" color="#7159c1" size={24} />
-                  <InfoTitle>{`Encomenda ${item.id}`}</InfoTitle>
-                </InfoHeader>
-                <EncomendaStatus>
-                  <LinhaStatus />
-                  <StatusRowBox>
-                    <StatusBox>
-                      <StatusPoint />
-                      <StatusText>Aguardando Retirada</StatusText>
-                    </StatusBox>
-                    <StatusBox>
-                      <StatusPoint />
-                      <StatusText>Retirada</StatusText>
-                    </StatusBox>
-                    <StatusBox>
-                      <StatusPoint />
-                      <StatusText>Entregue</StatusText>
-                    </StatusBox>
-                  </StatusRowBox>
-                </EncomendaStatus>
-                <EncomendaFooter>
-                  <FooterBlock>
-                    <Label>Data</Label>
-                    <LabelText>{item.data}</LabelText>
-                  </FooterBlock>
-                  <FooterBlock>
-                    <Label>Cidade</Label>
-                    <LabelText>{item.cidade}</LabelText>
-                  </FooterBlock>
-                  <DetalhesButton
-                    onPress={() => {
-                      this.handleDetalhes(item);
-                    }}
-                  >
-                    <DetalhesButtonText>Ver detalhes</DetalhesButtonText>
-                  </DetalhesButton>
-                </EncomendaFooter>
-              </Encomenda>
-            )}
-          />
-        </EntregaList>
-      </Container>
-    );
   }
+
+  async function fetchData(filter) {
+    const urlReq = `/encomendas?entregadorId=${id}`;
+    const urlFilter = `${urlReq}&${filter}`;
+    const urlMounted = filter ? urlFilter : urlReq;
+    const res = await api.get(urlMounted);
+    if (res.data) {
+      setEncomendaList([...res.data]);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  });
+
+  return (
+    <Container>
+      <Header>
+        <Image>
+          <ImageText>GA</ImageText>
+        </Image>
+        <Welcome>
+          <Text>Bem vindo de volta,</Text>
+          <NameText>{nome}</NameText>
+        </Welcome>
+        <Icon
+          name="exit-to-app"
+          color="#E74040"
+          size={24}
+          onPress={handleExit}
+        />
+      </Header>
+
+      <EntregaList>
+        <ListHeader>
+          <ListTitle>Entregas</ListTitle>
+          <ListFilter>
+            <FilterText onPress={handleChangeFilter('pendente')}>
+              Pendentes
+            </FilterText>
+            <FilterText onPress={handleChangeFilter('entregue')}>
+              Entregues
+            </FilterText>
+          </ListFilter>
+        </ListHeader>
+        <List
+          data={encomendaList}
+          keyExtractor={(encomenda) => encomenda.id}
+          renderItem={({ item }) => (
+            <Encomenda>
+              <InfoHeader>
+                <Icon name="local-shipping" color="#7159c1" size={24} />
+                <InfoTitle>{`Encomenda ${item.id}`}</InfoTitle>
+              </InfoHeader>
+              <EncomendaStatus>
+                <LinhaStatus />
+                <StatusRowBox>
+                  <StatusBox>
+                    <StatusPoint />
+                    <StatusText>Aguardando Retirada</StatusText>
+                  </StatusBox>
+                  <StatusBox>
+                    <StatusPoint />
+                    <StatusText>Retirada</StatusText>
+                  </StatusBox>
+                  <StatusBox>
+                    <StatusPoint />
+                    <StatusText>Entregue</StatusText>
+                  </StatusBox>
+                </StatusRowBox>
+              </EncomendaStatus>
+              <EncomendaFooter>
+                <FooterBlock>
+                  <Label>Data</Label>
+                  <LabelText>{item.data}</LabelText>
+                </FooterBlock>
+                <FooterBlock>
+                  <Label>Cidade</Label>
+                  <LabelText>{item.cidade}</LabelText>
+                </FooterBlock>
+                <DetalhesButton
+                  onPress={() => {
+                    handleDetalhes(item);
+                  }}
+                >
+                  <DetalhesButtonText>Ver detalhes</DetalhesButtonText>
+                </DetalhesButton>
+              </EncomendaFooter>
+            </Encomenda>
+          )}
+        />
+      </EntregaList>
+    </Container>
+  );
 }
 Dashboard.propTypes = {
   navigation: PropTypes.shape({
