fd09c9f9297d75b883ebdbdbf64bf7c2ae5c38a5
Alex Rabelo Ferreira <arf92@live.com>
2020-07-12 15:54:17 -0300

    Adicao de estilizacao de carrinho

--- go-stack/fase-4_redux-hooks/modulo07/src/pages/Cart/index.js
+++ go-stack/fase-4_redux-hooks/modulo07/src/pages/Cart/index.js
@@ -1,9 +1,75 @@
 import React from 'react';
+import {
+    MdRemoveCircleOutline,
+    MdAddCircleOutline,
+    MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 function Cart() {
-    return <div />;
+    return (
+        <Container>
+            <ProductTable>
+                <thead>
+                    <tr>
+                        <th />
+                        <th>Produto</th>
+                        <th>Qtd</th>
+                        <th>Sub total</th>
+                        <th />
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img src="teste.png" alt="produto" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <strong>Tenis muito massa</strong>
+                            <span>R$ 129,34</span>
+                        </td>
+                        <td>
+                            <div>
+                                <button type="button">
+                                    <MdRemoveCircleOutline
+                                        size={20}
+                                        color="#7159c1"
+                                    />
+                                </button>
+                                <input type="number" readOnly value={1} />
+                                <button type="button">
+                                    <MdAddCircleOutline
+                                        size={20}
+                                        color="#7159c1"
+                                    />
+                                </button>
+                            </div>
+                        </td>
+                        <td>
+                            <strong>R$ 123,45</strong>
+                        </td>
+                        <td>
+                            <button type="button">
+                                <MdDelete size={20} color="#7159c1" />
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </ProductTable>
+
+            <footer>
+                <button type="button">Finalizar pedido</button>
+
+                <Total>
+                    <span>Total</span>
+                    <strong>R$ 120,23</strong>
+                </Total>
+            </footer>
+        </Container>
+    );
 }
 
 export default Cart;
