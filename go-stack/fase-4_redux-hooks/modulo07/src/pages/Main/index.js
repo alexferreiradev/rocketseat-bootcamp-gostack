008d3a322924fc36e953bd5f126b860cc5e4ccb2
Alex Rabelo Ferreira <arf92@live.com>
2020-07-14 10:50:46 -0300

    Estilizacao dee carrinho

--- go-stack/fase-4_redux-hooks/modulo07/src/pages/Main/index.js
+++ go-stack/fase-4_redux-hooks/modulo07/src/pages/Main/index.js
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 
 import { ProductList } from './styles';
 
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
+
 function Main() {
+    const [produtoList, setProdutoList] = useState([]);
+
+    async function fetchData() {
+        const res = api.get('/products');
+        const data = res.data.map((item) => {
+            return {
+                ...item,
+                priceFormatted: formatPrice(item.price),
+            };
+        });
+        setProdutoList(data);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
     return (
         <ProductList>
-            <li>
-                <img src="teste.png" alt="Tenis" />
-                <strong>Tenis muito legal</strong>
-                <span>R$129</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
+            {produtoList.map((prod) => {
+                return (
+                    <li key={prod.id}>
+                        <img src={prod.image} alt={prod.title} />
+                        <strong>{prod.title}</strong>
+                        <span>{prod.price}</span>
+                        <button type="button">
+                            <div>
+                                <MdAddShoppingCart size={16} color="#fff" /> 3
+                            </div>
 
-                    <span>Adicionar ao carrinho</span>
-                </button>
-            </li>
+                            <span>Adicionar ao carrinho</span>
+                        </button>
+                    </li>
+                );
+            })}
         </ProductList>
     );
 }
