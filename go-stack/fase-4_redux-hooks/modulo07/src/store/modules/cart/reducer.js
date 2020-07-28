25181de474ad08d4cd5410e52c95d2d75c582c61
Alex Rabelo Ferreira <arf92@live.com>
2020-07-28 09:58:34 -0300

    Adicao de verificacao de estoque

--- go-stack/fase-4_redux-hooks/modulo07/src/store/modules/cart/reducer.js
+++ go-stack/fase-4_redux-hooks/modulo07/src/store/modules/cart/reducer.js
@@ -4,15 +4,7 @@ export default function cart(state = [], action) {
     switch (action.type) {
         case '@cart/add-success': {
             return produce(state, (draft) => {
-                const productIndex = draft.findIndex(
-                    (p) => p.id === action.payload.id
-                );
-
-                if (productIndex >= 0) {
-                    draft[productIndex].amount += 1;
-                } else {
-                    draft.push({ ...action.payload, amount: 1 });
-                }
+                draft.push(action.payload);
             });
         }
         case '@cart/remove': {
--- go-stack/fase-4_redux-hooks/modulo07/src/store/modules/cart/sagas.js
+++ go-stack/fase-4_redux-hooks/modulo07/src/store/modules/cart/sagas.js
@@ -1,11 +1,38 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, put, all, takeLatest, select } from 'redux-saga/effects';
 import api from '../../../services/api';
-import { addToCartSuccess } from './actions';
+import { addToCartSuccess, updateAmount } from './actions';
+import { formatPrice } from '../../../util/format';
 
 function* addToCart({ payload }) {
-    const res = yield call(api.get, `products/${payload.id}`);
-    if (res.data) {
-        yield put(addToCartSuccess(res.data));
+    const { id } = payload;
+    const productFound = yield select((state) =>
+        state.cart.find((p) => p.id === id)
+    );
+
+    const stockRes = yield call(api.get, `stock/${id}`);
+    const stockAmount = stockRes.data.amount;
+    if (stockAmount === 0) {
+        console.tron.warn('Stock indisponivel');
+    }
+
+    if (productFound) {
+        if (productFound.amount >= stockAmount) {
+            console.tron.warn('Stock indisponivel');
+        } else {
+            yield put(updateAmount(productFound.id, 1));
+        }
+    } else {
+        const res = yield call(api.get, `products/${id}`);
+        const { data } = res;
+        if (data) {
+            yield put(
+                addToCartSuccess({
+                    ...data,
+                    amount: 1,
+                    priceFormatted: formatPrice(data.price),
+                })
+            );
+        }
     }
 }
 
