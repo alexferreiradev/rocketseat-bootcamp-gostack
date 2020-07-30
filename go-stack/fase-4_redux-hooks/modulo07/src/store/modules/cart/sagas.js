707f77c54d94a5b67dc5e6012aef57f7bf644c12
Alex Rabelo Ferreira <arf92@live.com>
2020-07-30 07:31:42 -0300

    Adicao de navegacao pelo saga

--- go-stack/fase-4_redux-hooks/modulo07/src/store/modules/cart/sagas.js
+++ go-stack/fase-4_redux-hooks/modulo07/src/store/modules/cart/sagas.js
@@ -1,6 +1,7 @@
 import { call, put, all, takeLatest, select } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 import api from '../../../services/api';
+import history from '../../../services/history';
 import { addToCartSuccess, updateAmountSuccess } from './actions';
 import { formatPrice } from '../../../util/format';
 
@@ -33,6 +34,7 @@ function* addToCart({ payload }) {
                     priceFormatted: formatPrice(data.price),
                 })
             );
+            history.push('/cart');
         }
     }
 }
