d41ad55623797cc5ef7c32b8e7361e1a4cf0340d
Alex Rabelo Ferreira <arf92@live.com>
2020-07-30 08:25:26 -0300

    Adicao de eslint para react hooks

--- go-stack/fase-4_redux-hooks/modulo07/.eslintrc.js
+++ go-stack/fase-4_redux-hooks/modulo07/.eslintrc.js
@@ -22,8 +22,9 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: [
-    // 'react',
+    'react',
     'prettier',
+    'react-hooks',
   ],
   rules: {
     'prettier/prettier': 'error',
@@ -37,5 +38,7 @@ module.exports = {
     ],
     'import/prefer-default-export': 'off',
     'no-console': ['error', {allow: ['tron']}],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
