475ba3059dc9512594552af3d8c01adac38ffdf3
Alex Rabelo Ferreira <arf92@live.com>
2020-08-15 11:34:45 -0300

    Adicao de estilizacao signIn

--- go-stack/fase-5_finalizacao_go-barber/modulo09/src/pages/SignIn/index.js
+++ go-stack/fase-5_finalizacao_go-barber/modulo09/src/pages/SignIn/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
+import logo from '~/assets/logo.svg';
 // import { Container } from './styles';
 
 function SignIn() {
-  return <h1>Login</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Senha" />
+        <button type="submit">Acessar</button>
+        <Link to="/register">Cria conta gratuíta</Link>
+      </form>
+    </>
+  );
 }
 
 export default SignIn;
--- go-stack/fase-5_finalizacao_go-barber/modulo09/src/pages/_layouts/auth/index.js
+++ go-stack/fase-5_finalizacao_go-barber/modulo09/src/pages/_layouts/auth/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Wrapper } from './styles';
+import { Wrapper, Content } from './styles';
 
 function Auth({ children }) {
-  return <Wrapper>{children}</Wrapper>;
+  return (
+    <Wrapper>
+      <Content>{children}</Content>
+    </Wrapper>
+  );
 }
 
 Auth.propTypes = {
