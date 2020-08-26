5c11de027cb580aa271dcf98c00f1c70a76b927a
Alex Rabelo Ferreira <arf92@live.com>
2020-08-26 07:12:21 -0300

    Adicao de validação para formularios de auth

--- go-stack/fase-5_finalizacao_go-barber/modulo09/src/pages/SignIn/index.js
+++ go-stack/fase-5_finalizacao_go-barber/modulo09/src/pages/SignIn/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
+const schema = Yup.object.shape({
+  email: Yup.string()
+    .email('Insira um email válido')
+    .required('Email é requerido'),
+  password: Yup.string().required('Senha é requerida'),
+});
+
 function SignIn() {
+  function handleSubmit() {}
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Senha" />
+      <Form onSubmit={handleSubmit} shema={schema}>
+        <Input type="email" name="email" placeholder="Email" />
+        <Input type="password" name="email" placeholder="Senha" />
         <button type="submit">Acessar</button>
         <Link to="/register">Cria conta gratuíta</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- go-stack/fase-5_finalizacao_go-barber/modulo09/src/pages/SignUp/index.js
+++ go-stack/fase-5_finalizacao_go-barber/modulo09/src/pages/SignUp/index.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
+const schema = Yup.object.shape({
+  name: Yup.string().required('Nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um email válido')
+    .required('Email é requerido'),
+  password: Yup.string().required('Senha é requerida'),
+});
+
 function SignUp() {
+  function handleSubmit() {}
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form>
-        <input placeholder="Nome completo" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Senha" />
+      <Form onSubmit={handleSubmit} schema={schema}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Email" />
+        <Input name="password" type="password" placeholder="Senha" />
         <button type="submit">Criar conta</button>
         <Link to="/">Já tenho login</Link>
-      </form>
+      </Form>
     </>
   );
 }
