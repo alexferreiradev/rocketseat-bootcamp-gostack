e5c6566357db05a195e94146f2080f5c7d3791da
Alex Rabelo Ferreira <arf92@live.com>
2020-10-23 10:17:24 -0300

    fix: login

--- go-stack/fase-5_finalizacao_go-barber/modulo09/src/store/modules/user/reducer.js
+++ go-stack/fase-5_finalizacao_go-barber/modulo09/src/store/modules/user/reducer.js
@@ -7,7 +7,7 @@ const INITIAL_STATE = {
 export default function auth(state = INITIAL_STATE, action) {
   return produce(state, (draft) => {
     switch (action.type) {
-      case '@auth/': {
+      case '@auth/SIGN_IN_SUCCESS': {
         draft.profile = action.payload.user;
         break;
       }
