d77efcd036a163028544b9fe634f1beb4b89ca11
Alex Rabelo Ferreira <arf92@live.com>
2020-12-02 19:22:29 -0300

    add: config redux

---
+++ go-stack/fase-5_finalizacao_go-barber/modulo10/src/store/modules/auth/sagas.js
@@ -0,0 +1,69 @@
+import { all, takeLatest, call, put } from 'redux-saga/effects';
+import { Alert } from 'react-native';
+
+import api from '~/services/api';
+
+import { singInSuccess, singFailure } from './actions';
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+    const res = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = res.data;
+    if (user.provider) {
+      Alert.alert('Falha', 'Usuario não é prestador');
+      return;
+    }
+
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
+    yield put(singInSuccess(token, user));
+  } catch (err) {
+    Alert.alert('Falha', 'Falha na autenticação, verifique seus dados');
+    yield put(singFailure());
+  }
+}
+
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+    });
+
+    // history.push('/');
+  } catch (error) {
+    Alert.alert('Falha', 'Falha no cadastro, verifique seus dados');
+    if (error.response.data.message) {
+      Alert.alert('Falha', error.response.data.message);
+    }
+    yield put(singFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export function signOut() {
+  // history.push('/');
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+  takeLatest('@auth/SIGN_OUT', signOut),
+  takeLatest('persist/REHYDRATE', setToken),
+]);
