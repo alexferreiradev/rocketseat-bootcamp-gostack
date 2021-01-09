5f184d189233d8e9bce4b2e64bc9c3f843971b02
Alex Rabelo Ferreira <arf92@live.com>
2021-01-09 11:41:53 -0300

    fix: component backgroud
    - estava sem export default gerando erro

--- go-stack/fase-5_finalizacao_go-barber/modulo10/src/components/Background/index.js
+++ go-stack/fase-5_finalizacao_go-barber/modulo10/src/components/Background/index.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components/native';
 import LinearGradient from 'react-native-linear-gradient';
 
-export const Container = styled(LinearGradient).attrs({
+export default styled(LinearGradient).attrs({
   colors: ['#7159c1', '#ab59c1'],
 })`
   flex: 1;
