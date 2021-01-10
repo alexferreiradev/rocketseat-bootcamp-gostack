e87430d63a347489667d779b232ca47d21353afb
Alex Rabelo Ferreira <arf92@live.com>
2021-01-10 08:24:51 -0300

    fix: erro de string como texto
    - remocao de ';' que estava a frente de <Routes/>

--- go-stack/fase-5_finalizacao_go-barber/modulo10/src/index.js
+++ go-stack/fase-5_finalizacao_go-barber/modulo10/src/index.js
@@ -14,7 +14,7 @@ const App = () => {
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-        <Routes />;
+        <Routes />
       </PersistGate>
     </Provider>
   );
