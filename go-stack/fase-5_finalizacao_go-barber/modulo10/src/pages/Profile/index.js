bde70cd4b577120f36c99e9966225ecdd5a1b141
Alex Rabelo Ferreira <arf92@live.com>
2021-01-18 10:30:18 -0300

    add: funcoes da pagina de perfil

--- go-stack/fase-5_finalizacao_go-barber/modulo10/src/pages/Profile/index.js
+++ go-stack/fase-5_finalizacao_go-barber/modulo10/src/pages/Profile/index.js
@@ -1,21 +1,123 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Background from '~/components/Background';
 
-import { Container } from './styles';
+import {
+  Container,
+  Title,
+  Form,
+  FormInput,
+  SubmitButton,
+  Separator,
+} from './styles';
+
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
 const Profile = () => {
+  const profile = useSelector((state) => state.user.profile);
+
+  const emailRef = useRef();
+  const oldPasswordRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+
+  const [name, setName] = useState(profile.name);
+  const [email, setEmail] = useState(profile.email);
+  const [oldPassword, setOldPassword] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  function handleSubmit() {
+    dispatch(
+      updateProfileRequest(name, email, oldPassword, password, confirmPassword)
+    );
+  }
+
+  useEffect(() => {
+    setOldPassword('');
+    setPassword('');
+    setConfirmPassword('');
+  }, [profile]);
+
   return (
     <Background>
-      <Text>Teste profile</Text>
+      <Container>
+        <Title>Meu perfil</Title>
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCaptalize="none"
+            placeholder="Digite seu nome"
+            onSubmitEditing={() => emailRef.current.focus()}
+            returnKeyType="next"
+            value={name}
+            onChangeText={setName}
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCaptalize="none"
+            placeholder="Digite seu e-mail"
+            ref={emailRef}
+            returnKeyType="next"
+            onSubmitEditing={() => oldPasswordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
+          />
+
+          <Separator />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha antiga"
+            ref={oldPasswordRef}
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+            value={oldPassword}
+            onChangeText={setOldPassword}
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua nova senha"
+            ref={passwordRef}
+            returnKeyType="next"
+            onSubmitEditing={() => confirmPasswordRef.current.focus()}
+            value={password}
+            onChangeText={setPassword}
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Confirme sua senha"
+            ref={confirmPasswordRef}
+            returnKeyType="send"
+            onSubmitEditing={() => handleSubmit()}
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+          />
+
+          <SubmitButton loading={loading} onPress={handleSubmit}>
+            Atualizar perfil
+          </SubmitButton>
+        </Form>
+      </Container>
     </Background>
   );
 };
 
 Profile.navigationOptions = {
-  tabBarLabel: 'Agendamentos',
+  tabBarLabel: 'Meu Perfil',
   tabBarIcon: ({ tintColor }) => (
     <Icon name="event" size={20} color={tintColor} />
   ),
