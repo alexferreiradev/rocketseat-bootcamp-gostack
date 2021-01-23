4b359d7d67bb37664080735861da2c407bcc82a7
Alex Rabelo Ferreira <arf92@live.com>
2021-01-23 17:56:01 -0300

    add: datePicker por plataforma

---
+++ go-stack/fase-5_finalizacao_go-barber/modulo10/src/components/DateTimeInput/index.ios.js
@@ -0,0 +1,39 @@
+import React, { useState, useMemo } from 'react';
+import { DatePickerIOS } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import { Container, DateButton, DateText, Picker } from './styles';
+
+const DateTimeInput = ({ date, onChange }) => {
+  const [opened, setOpened] = useState(false);
+
+  const dateFormatted = useMemo(
+    () => format(date, "dd 'de' MMM 'de' yyyy", { locale: pt }),
+    [date]
+  );
+  return (
+    <Container>
+      <DateButton onPress={() => setOpened(true)}>
+        <Icon name="event" color="#fff" size={20} />
+        <DateText>{dateFormatted}</DateText>
+      </DateButton>
+
+      {opened && (
+        <Picker>
+          <DatePickerIOS
+            date={date}
+            onChange={onChange}
+            minimumDate={new Date()}
+            minuteInterval={60}
+            locale="pt"
+            mode="date"
+          />
+        </Picker>
+      )}
+    </Container>
+  );
+};
+
+export default DateTimeInput;
