8f0219269ade1569eb566f17effeed3d3aa390e0
Alex Rabelo Ferreira <arf92@live.com>
2021-01-23 18:32:09 -0300

    add: tela de confirmacao

--- go-stack/fase-5_finalizacao_go-barber/modulo10/src/pages/New/Confirm/index.js
+++ go-stack/fase-5_finalizacao_go-barber/modulo10/src/pages/New/Confirm/index.js
@@ -1,10 +1,59 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useMemo } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { formatRelative, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import api from '~/services/api';
+import Background from '~/components/Background';
 
-const Confirm = () => {
-  return <View />;
+import { Container, Avatar, Name, Time, SubmitButton } from './styles';
+
+const Confirm = ({ navigation }) => {
+  const provider = navigation.getParam('provider');
+  const time = navigation.getParam('time');
+
+  const dateFormatted = useMemo(() =>
+    formatRelative(parseISO(time), new Date(), { locale: pt })
+  );
+
+  async function handleAddAppointment() {
+    const res = await api.post('appointments', {
+      provider_id: provider.id,
+      date: time,
+    });
+
+    navigation.navigate('Dashboard');
+  }
+  return (
+    <Background>
+      <Container>
+        <Avatar
+          source={{
+            uri: provider.avatar
+              ? provider.avatar.url
+              : `https://api.adorable.io/avatar/50/${provider.name}`,
+          }}
+        />
+
+        <Name>{provider.name}</Name>
+        <Time>{dateFormatted}</Time>
+
+        <SubmitButton onPress={() => handleAddAppointment()}>
+          Confirmar Agendamento
+        </SubmitButton>
+      </Container>
+    </Background>
+  );
 };
+
+Confirm.navigationOptions = ({ navigation }) => ({
+  title: 'Confirmar agendamento',
+  headerLeft: () => (
+    <TouchableOpacity onPress={() => navigation.goBack()}>
+      <Icon name="chevron-left" size={20} color="#fff" />
+    </TouchableOpacity>
+  ),
+});
 
 export default Confirm;
