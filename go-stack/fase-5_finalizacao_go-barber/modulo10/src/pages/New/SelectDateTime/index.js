edfe9aec8d666628ebf77600b1f3733ea6275621
Alex Rabelo Ferreira <arf92@live.com>
2021-01-23 18:13:04 -0300

    add: tela de selecionar horario

--- go-stack/fase-5_finalizacao_go-barber/modulo10/src/pages/New/SelectDateTime/index.js
+++ go-stack/fase-5_finalizacao_go-barber/modulo10/src/pages/New/SelectDateTime/index.js
@@ -1,18 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
+import api from '~/services/api';
 import DateTimeInput from '~/components/DateTimeInput';
-import { Container } from './styles';
+import { Container, HourList, Hour, Title } from './styles';
 
-const SelectDateTime = () => {
-  const [date, setDate] = useState();
+const SelectDateTime = ({ navigation }) => {
+  const [date, setDate] = useState(new Date());
+  const [hours, setHours] = useState([]);
+
+  const provider = navigation.getParam('provider');
+
+  useEffect(() => {
+    async function loadAvailable() {
+      const res = await api.get(`providers/${provider.id}/availablity`, {
+        params: {
+          date: date.getTime(),
+        },
+      });
+
+      setHours(res.data);
+    }
+
+    loadAvailable();
+  }, [date, provider.id]);
+
+  function handleSelectHour(time) {
+    navigation.navigate('Confirm', {
+      provider,
+      time,
+    });
+  }
 
   return (
     <Background>
       <Container>
         <DateTimeInput date={date} onChange={setDate} />
+
+        <HourList
+          data={hours}
+          keyExtractor={(item) => item.time}
+          renderItem={({ item }) => (
+            <Hour
+              enabled={item.available}
+              onPress={() => handleSelectHour(item.value)}
+            >
+              <Title>{item.time}</Title>
+            </Hour>
+          )}
+        />
       </Container>
     </Background>
   );
