91b2e1fe2188d75069d76aa37d8d1ecb7415756a
Alex Rabelo Ferreira <arf92@live.com>
2021-03-06 07:54:44 -0300

    fix: config production para database

---
+++ go-stack/fase-2_go-barber/go-barber/src/config/database.js
@@ -0,0 +1,20 @@
+require('dotenv/config');
+module.exports = {
+  development: {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    port: process.env.DB_PORT || 5432,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    protocol: process.env.DB_PROTOCOL,
+    define: {
+        timestamp: true,
+        underscored: true,
+        underscoredAll: true,
+    }
+  },
+  production: {
+    use_env_variable: DATABASE_URL
+  }
+};
