e396fcbb2159d47937614dbf448206bb9a534df7
Alex Rabelo Ferreira <arf92@live.com>
2021-03-24 06:14:18 -0300

    fix: url para ssl

--- go-stack/fase-5_finalizacao_go-barber/modulo09/src/services/api.js
+++ go-stack/fase-5_finalizacao_go-barber/modulo09/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://teste-go-barber.herokuapp.com',
+  baseURL: 'https://teste-go-barber.herokuapp.com',
 });
 
 export default api;
