3d865baa33002574d4b9de4a3dac8dc43797b39c
Alex Rabelo Ferreira <arf92@live.com>
2021-03-24 07:07:07 -0300

    add: url dinamica para api

--- go-stack/fase-5_finalizacao_go-barber/modulo09/src/services/api.js
+++ go-stack/fase-5_finalizacao_go-barber/modulo09/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import runtimeEnv from '@mars/heroku-js-runtime-env';
 
+const env = runtimeEnv();
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: env.REACT_APP_API_URL,
 });
 
 export default api;
