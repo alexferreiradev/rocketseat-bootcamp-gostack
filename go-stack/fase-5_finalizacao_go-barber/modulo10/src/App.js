25b248fbd184b09aec0511db3eb5f8acdafff3a2
Alex Rabelo Ferreira <arf92@live.com>
2021-04-15 10:08:10 -0300

    add: config para navigation service

--- go-stack/fase-5_finalizacao_go-barber/modulo10/src/App.js
+++ go-stack/fase-5_finalizacao_go-barber/modulo10/src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 import createRouter from './routes';
+import NavigationService from './services/navigation';
 
 const App = () => {
   const signed = useSelector((state) => state.auth.signed);
   const Routes = createRouter(signed);
 
-  return <Routes />;
+  return (
+    <Routes
+      ref={(navigatorRef) => {
+        NavigationService.setTopLevelNavigator(navigatorRef);
+      }}
+    />
+  );
 };
 
 export default App;
